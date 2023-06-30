2adcd0e78dd9ac2436996bb887fdcb5a5222734a
Ethan <eman5797@gmail.com>
2023-06-30 18:27:37 -0400

    editing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,41 @@
-import { BottomNavigation, TextField } from "@mui/material";
+import SendOutlinedIcon from '@mui/icons-material/Send';
+import { BottomNavigation, IconButton, TextField } from "@mui/material";
 
 export default function Footer() {
     const footerDesign = {
         position: 'fixed',
         bottom: 0,
-        borderTop: '1px solid darkgrey',
+        borderTop: '1px solid rgba(1, 1, 1, .7)',
         width: 1.0,
         padding: '10px',
         height: 'fit-content',
+        display: 'flex',
+        alignItems: 'center'
     };
     const textboxDesign = {
-        width: '70vw'
+        width: '65vw'
+    };
+    const sendButtonDesign = {
+
     };
     return (
         <BottomNavigation
             sx={footerDesign}
         >
             <TextField
-            label="Have a thought?"
-            variant="standard"
-            multiline
-            sx={textboxDesign}
+                label="Have a thought?"
+                variant="standard"
+                multiline
+                sx={textboxDesign}
             />
+            <IconButton
+                color='secondary'
+            >
+                <SendOutlinedIcon
+                    fontSize='large'
+                    sx={sendButtonDesign}
+                />
+            </IconButton>
         </BottomNavigation>
     );
 }
