b804a944699117d45a97dde9c90d2c8c525b56be
Ethan <eman5797@gmail.com>
2023-06-29 23:50:17 -0400

    added an env example, and init firebase

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,25 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+// Importing Firebase into project
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// config firebase through env variables
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initializes Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
