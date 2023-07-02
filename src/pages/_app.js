f225665d7fa3f42a482ff1a8952dc1d050f0232a
Ethan <eman5797@gmail.com>
2023-07-01 21:22:50 -0400

    added redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,20 @@ import Layout from '@/src/components/Layout'
 import { ThemeProvider } from '@emotion/react'
 import theme from '../styles/theme/theme'
 import { CssBaseline } from '@mui/material'
+import { Provider } from 'react-redux';
+import configureStore from '../store';
+
+const store = configureStore();
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </Provider>
   )
 };
